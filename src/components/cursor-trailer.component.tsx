fcf0aab1d44199fb501f3f6e73abc653bbe4931f
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-03-11 21:43:23 +0200

    Fixed cursor trailer render lag

--- src/components/cursor-trailer.component.tsx
+++ src/components/cursor-trailer.component.tsx
@@ -1,6 +1,8 @@
 import {
   Fragment,
+  useCallback,
   useEffect,
+  useMemo,
   useRef,
   useState,
 } from "react";
@@ -18,19 +20,38 @@ const CursorTrailer = () => {
       y: 0,
     });
 
-  const [hasFocus, setHasFocus] =
-    useState<boolean>(true);
-
   const circlesRef = useRef<
     (HTMLDivElement | null)[]
   >([]);
   const circles = circlesRef.current;
 
-  const circleStyle = {
-    transition: "all 0.1s ease-out",
-    transform: "scale(1)",
-    display: "none",
-  };
+  const circleStyle = useMemo(
+    () => ({
+      transition: "all 0.1s ease-out",
+      transform: "scale(1)",
+      display: "none",
+    }),
+    []
+  );
+
+  const animateCircles = useCallback(() => {
+    let x = coordinates.x;
+    let y = coordinates.y;
+
+    circles.forEach((circle, index) => {
+      if (circle) {
+        circle.style.display = "block";
+        circle.style.left = `${x - 8}px`;
+        circle.style.top = `${y - 8}px`;
+
+        const nextCircle =
+          circles[index + 1] || circles[0];
+
+        x += (nextCircle!.offsetLeft - x) * 0.02;
+        y += (nextCircle!.offsetTop - y) * 0.02;
+      }
+    });
+  }, [coordinates, circles]);
 
   useEffect(() => {
     if (!circles) return;
@@ -45,7 +66,7 @@ const CursorTrailer = () => {
 
     const debouncedUpdateCoordinates = debounce(
       handleMouseMove,
-      16 // Update every 16ms (60fps)
+      2 // Update every 16ms (60fps)
     );
 
     window.addEventListener(
@@ -53,75 +74,26 @@ const CursorTrailer = () => {
       debouncedUpdateCoordinates
     );
 
-    const handleWindowBlur = () => {
-      setHasFocus(false);
-    };
-
-    const handleWindowFocus = () => {
-      setHasFocus(true);
-    };
-
-    window.addEventListener(
-      "blur",
-      handleWindowBlur
-    );
-    window.addEventListener(
-      "focus",
-      handleWindowFocus
-    );
-
     return () => {
       window.removeEventListener(
         "mousemove",
         debouncedUpdateCoordinates
       );
-      window.removeEventListener(
-        "blur",
-        handleWindowBlur
-      );
-      window.removeEventListener(
-        "focus",
-        handleWindowFocus
-      );
     };
-  }, []);
+  }, [circles, animateCircles]);
 
-  useEffect(() => {
-    if (!circles) return;
-    if (!circles.length) return;
-
-    const animateCircles = () => {
-      let x = coordinates.x;
-      let y = coordinates.y;
-
-      circles.forEach((circle, index) => {
-        if (circle) {
-          circle.style.display = "block";
-          circle.style.left = `${x - 10}px`;
-          circle.style.top = `${y - 10}px`;
-
-          const nextCircle =
-            circles[index + 1] || circles[0];
-
-          x += (nextCircle!.offsetLeft - x) * 0.2;
-          y += (nextCircle!.offsetTop - y) * 0.2;
-        }
-      });
-    };
-
-    window.requestAnimationFrame(animateCircles);
-  }, [coordinates, hasFocus]);
+  window.requestAnimationFrame(animateCircles);
 
   return (
     <Fragment>
       {[...Array(20)].map((_, index) => (
         <div
           key={index}
-          className="h-5 w-5 rounded-full bg-yellow opacity-2 fixed pointer-events-none z-9999"
+          className="h-5 w-5 rounded-full bg-yellow opacity-2 fixed pointer-events-none z-999"
           style={{
             ...circleStyle,
             transform: `scale(${
-              (circles.length - index) * 0.05
+              (circles.length - index) / 25
             })`,
           }}
           ref={(el) =>
