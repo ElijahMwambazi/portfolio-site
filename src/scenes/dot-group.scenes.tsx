2b4951e31b2abecbbb65913faaeff16f80c4f395
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-03-13 12:14:28 +0200

    Added z-index

--- src/scenes/dot-group.scenes.tsx
+++ src/scenes/dot-group.scenes.tsx
@@ -14,7 +14,7 @@ const DotGroup = ({
   selectedPage,
   setSelectedPage,
 }: DotGroupProps) => {
-  const selectedStyles = `relative bg-yellow w-3 h-3 rounded-full before:absolute before:w-6 before:h-6 before:rounded-full before:border-2 before:border-yellow before:left-[-50%] before:top-[-50%]`;
+  const selectedStyles = `relative bg-yellow w-3 h-3 rounded-full before:absolute before:w-6 before:h-6 before:rounded-full before:border-2 before:border-yellow before:left-[-50%] before:top-[-50%] z-2`;
 
   return (
     <div className="flex flex-col gap-6 fixed top-[60%] right-7">
