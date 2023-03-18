912ede2563e9239b8cf50d176db563f48d275c07
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-03-19 00:06:08 +0200

    Added heading component

---
+++ src/components/heading.component.tsx
@@ -0,0 +1,30 @@
+import Border from "./border.component";
+
+export type HeadingProps = {
+  firstPartOfText: string;
+  secondPartOfText: string;
+};
+
+const Heading = ({
+  firstPartOfText,
+  secondPartOfText,
+}: HeadingProps) => {
+  return (
+    <div className="">
+      <p className="font-bungee font-semibold text-2xl">
+        {firstPartOfText}
+        <span className="text-dark-gray">
+          {secondPartOfText}
+        </span>
+      </p>
+      {
+        <Border
+          width={""}
+          className="-mt-1 h-2 bg-white rounded"
+        />
+      }
+    </div>
+  );
+};
+
+export default Heading;
