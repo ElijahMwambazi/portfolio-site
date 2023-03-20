4b2996f2cfa0d42b2ec82eb441371914905aefc5
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-03-20 16:42:28 +0200

    Changed it to render at initial mount

--- src/scenes/particles.scene.tsx
+++ src/scenes/particles.scene.tsx
@@ -1,21 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const randomNumber = (limit: number) => {
   return Math.floor(Math.random() * limit);
 };
 
+export type Particle = {
+  key: number;
+  particleClasses: string[];
+  particleOpacity: number;
+  heightWidth: number;
+  startLocation: number;
+  timeDelay: number;
+  animationDuration: number;
+};
+
 const Particles = () => {
-  return (
-    <div className="relative bg-gray-100 bg-opacity-60 z-10 h-5 w-full mt-20">
-      {[...Array(200)].map((_, index) => {
-        const particleClasses: string[] = [];
+  const [particles, setParticles] = useState<
+    Particle[]
+  >([]);
+
+  useEffect(() => {
+    const newParticles = [...Array(200)].map(
+      (_, index) => {
+        const particleClasses = [];
         const particleOpacity =
           randomNumber(10) * 0.1;
         const heightWidth = randomNumber(11) + 5;
         const startLocation =
           -50 + randomNumber(150);
         const timeDelay = randomNumber(10);
-        const animDuration =
+        const animationDuration =
           5 +
           randomNumber(10) +
           Math.random() +
@@ -39,23 +53,39 @@ const Particles = () => {
           );
         }
 
-        return (
-          <div
-            key={index}
-            className={`${particleClasses.join(
-              " "
-            )} absolute bg-yellow rounded-full pointer-events-none z-20`}
-            style={{
-              width: `${heightWidth}px`,
-              height: `${heightWidth}px`,
-              left: `${startLocation}%`,
-              opacity: `${particleOpacity}`,
-              animationDelay: `${timeDelay}s`,
-              animationDuration: `${animDuration}s`,
-            }}
-          />
-        );
-      })}
+        return {
+          key: index,
+          particleClasses,
+          heightWidth,
+          startLocation,
+          particleOpacity,
+          timeDelay,
+          animationDuration,
+        };
+      }
+    );
+
+    setParticles(newParticles);
+  }, []);
+
+  return (
+    <div className="relative bg-gray-100 bg-opacity-60 z-10 h-5 w-full mt-20">
+      {particles.map((particle) => (
+        <div
+          key={particle.key}
+          className={`${particle.particleClasses.join(
+            " "
+          )} absolute bg-yellow rounded-full pointer-events-none z-20`}
+          style={{
+            width: `${particle.heightWidth}px`,
+            height: `${particle.heightWidth}px`,
+            left: `${particle.startLocation}%`,
+            opacity: `${particle.particleOpacity}`,
+            animationDelay: `${particle.timeDelay}s`,
+            animationDuration: `${particle.animationDuration}s`,
+          }}
+        />
+      ))}
     </div>
   );
 };
