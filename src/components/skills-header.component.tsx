8cd0ef30a5ba9b7a02b85bf57813a63f61834278
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-04-03 12:32:54 +0200

    Added arrow icon

--- src/components/skills-header.component.tsx
+++ src/components/skills-header.component.tsx
@@ -1,4 +1,5 @@
 import RandomizedText from "./randomized-text.componenet";
+import { ReactComponent as ArrowIcon } from "../assets/arrow.svg";
 
 export type SkillsHeaderProps = {
   heading: string;
@@ -14,17 +15,34 @@ const SkillsHeader = ({
   onSKillClick,
 }: SkillsHeaderProps) => {
   return (
-    <RandomizedText
-      textToRandomize={heading}
-      singleWord={false}
-      className={`font-playfair font-bold mt-10 text-2xl
-      text-justify md:text-start ${
+    <div
+      className={`mt-10 ${
         skill
           ? "text-yellow mb-7"
           : "text-dark-gray"
-      } hover:text-yellow border-2 rounded p-5`}
+      } hover:text-yellow border-2 rounded p-5 flex justify-between items-center z-50`}
       onClick={onSKillClick}
-    />
+    >
+      <RandomizedText
+        textToRandomize={heading}
+        singleWord={false}
+        className={`font-playfair font-bold text-2xl
+      text-justify md:text-start `}
+      />
+      <ArrowIcon
+        className={`${
+          skill ? "fill-yellow" : "fill-dark-gray"
+        }`}
+        style={{
+          transform: `${
+            skill
+              ? "rotate(180deg)"
+              : "rotate(360deg)"
+          }`,
+          transition: "all 0.3s linear",
+        }}
+      />
+    </div>
   );
 };
 
