deabe64af769d1572c31e6370f724d9fdd9ad043
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-04-04 10:18:19 +0200

    Cleaned up styles

--- src/components/skills-card-container.componenet.tsx
+++ src/components/skills-card-container.componenet.tsx
@@ -14,13 +14,8 @@ const SkillCardsContainer = ({
       className={`grid grid-cols-3 gap-6 w-full px-10`}
       style={{
         display: `${showSkills ? "" : "none"}`,
-        visibility: `${
-          showSkills ? "visible" : "hidden"
-        }`,
         animation: `${
-          showSkills
-            ? "dropDown 0.3s linear"
-            : "goUp 0.3s linear"
+          showSkills && "dropDown 0.3s linear"
         }`,
       }}
     >
