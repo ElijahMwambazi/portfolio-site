8f0f78f8a0da851a797a6c54e0d9ebbac6dd7270
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-04-06 09:57:35 +0200

    Random addition for projects page

---
+++ src/components/projects-container.component.tsx
@@ -0,0 +1,10 @@
+import Project, {
+  ProjectProps,
+} from "./project.component";
+import { ReactComponent as GitHubIcon } from "../assets/socials/github.svg";
+
+const ProjectsContainer = () => {
+  return <div></div>;
+};
+
+export default ProjectsContainer;
---
+++ src/scenes/projects.scene.tsx
@@ -0,0 +1,53 @@
+import { motion } from "framer-motion";
+import useMediaQuery from "../hooks/useMediaQuery";
+import Heading from "../components/heading.component";
+// import ProjectsContainer from "../components/projects-container.component";
+
+const Projects = () => {
+  const isAboveMediumScreens = useMediaQuery(
+    "(min-width: 1060px"
+  );
+
+  return (
+    <section
+      id="projects"
+      className="relative mt-24 pb-24"
+    >
+      <motion.div
+        className="md:w-1/3"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 1 }}
+        variants={{
+          hidden: { opacity: 0, x: -100 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
+        <div className="flex">
+          <Heading
+            firstPartOfText={"pro"}
+            secondPartOfText={"jects"}
+          />
+        </div>
+      </motion.div>
+
+      {/* Projects Container */}
+      <motion.div
+        className="md:w-1/3"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 1 }}
+        variants={{
+          hidden: { opacity: 0, x: -100 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
+        {/* <ProjectsContainer projects={[]} /> */}
+      </motion.div>
+    </section>
+  );
+};
+
+export default Projects;
