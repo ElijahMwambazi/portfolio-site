087de68ce099aab94c63c1e231194d796022fd97
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-04-10 12:11:17 +0200

    Added filler projects

--- src/components/projects-container.component.tsx
+++ src/components/projects-container.component.tsx
@@ -1,16 +1,54 @@
-import Project, {
-  ProjectProps,
-} from "./project.component";
+import Project from "./project.component";
 
 const ProjectsContainer = () => {
   return (
-    <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-6">
+    <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-20 mt-10">
+      {/* Project 1 */}
       <Project
-        imgSrc={""}
-        title={""}
-        description={""}
+        imgSrc={
+          "../../public/projects/project-1.jpeg"
+        }
+        projectName={"Project 1"}
+        description={
+          "The purpose of this project is to provide you with experience in mapping between phenomena in the real world (and data) and the possible distribution functions"
+        }
         isActive={false}
         href={""}
+        projectState={"TODO"}
+        startDate={"TBD"}
+        finishDate={"TBD"}
+      />
+
+      {/* Project 2 */}
+      <Project
+        imgSrc={
+          "../../public/projects/project-2.jpeg"
+        }
+        projectName={"Project 2"}
+        description={
+          "The purpose of this project is to provide you with experience in mapping between phenomena in the real world (and data) and the possible distribution functions"
+        }
+        isActive={true}
+        href={""}
+        projectState={"DONE"}
+        startDate={"TBD"}
+        finishDate={"TBD"}
+      />
+
+      {/* Project 3 */}
+      <Project
+        imgSrc={
+          "../../public/projects/project-3.jpeg"
+        }
+        projectName={"Project 3"}
+        description={
+          "The purpose of this project is to provide you with experience in mapping between phenomena in the real world (and data) and the possible distribution functions"
+        }
+        isActive={true}
+        href={""}
+        projectState={"ACTIVE"}
+        startDate={"TBD"}
+        finishDate={"TBD"}
       />
     </div>
   );
