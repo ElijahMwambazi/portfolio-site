3df5b13d87562dcb8bab2a71bcd32509fc3b09e0
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2023-04-18 16:30:48 +0200

    Added a couple of actual projects

--- src/components/projects-container.component.tsx
+++ src/components/projects-container.component.tsx
@@ -4,51 +4,131 @@ const ProjectsContainer = () => {
   return (
     <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-20 mt-10">
       {/* Project 1 */}
+      {/* TODO: Change description, add start date and finish date, change image*/}
       <Project
         imgSrc={
           "../../public/projects/project-1.jpeg"
         }
-        projectName={"Project 1"}
+        projectName={"Note Daddy"}
         description={
           "The purpose of this project is to provide you with experience in mapping between phenomena in the real world (and data) and the possible distribution functions"
         }
+        techStack={["React", "TypeScript"]}
         isActive={false}
-        href={""}
         projectState={"TODO"}
         startDate={"TBD"}
         finishDate={"TBD"}
+        projectHref={""}
+        githubHref={
+          "https://github.com/ElijahMwambazi/note-daddy"
+        }
       />
 
       {/* Project 2 */}
+      {/* TODO: Change description, , add start date and finish date, change image*/}
       <Project
         imgSrc={
           "../../public/projects/project-2.jpeg"
         }
-        projectName={"Project 2"}
+        projectName={"Post Daddy"}
         description={
           "The purpose of this project is to provide you with experience in mapping between phenomena in the real world (and data) and the possible distribution functions"
         }
-        isActive={true}
-        href={""}
-        projectState={"DONE"}
+        techStack={["JavaScript", "CSS", "HTML"]}
+        isActive={false}
+        projectState={"TODO"}
         startDate={"TBD"}
         finishDate={"TBD"}
+        projectHref={""}
+        githubHref={""}
       />
 
       {/* Project 3 */}
+      {/* TODO: change image, add start date and finish date */}
       <Project
         imgSrc={
           "../../public/projects/project-3.jpeg"
         }
-        projectName={"Project 3"}
+        projectName={"Clothes Clothes Clothes"}
+        description={
+          "E-Commerece clothing web app, built with Redux for state management, styled components for styling, stripe for payments"
+        }
+        techStack={["React", "TypeScript"]}
+        isActive={true}
+        projectState={"DONE"}
+        startDate={"TBD"}
+        finishDate={"TBD"}
+        projectHref={
+          "https://adorable-strudel-5f2708.netlify.app/"
+        }
+        githubHref={
+          "https://github.com/ElijahMwambazi/clothes-clothes-clothes"
+        }
+      />
+
+      {/* Project 4 */}
+      {/* TODO: Change description, add start date and finish date, change image*/}
+      <Project
+        imgSrc={
+          "../../public/projects/project-3.jpeg"
+        }
+        projectName={"Trillo"}
         description={
           "The purpose of this project is to provide you with experience in mapping between phenomena in the real world (and data) and the possible distribution functions"
         }
+        techStack={["HTML", "Sass", "JavaScript"]}
         isActive={true}
-        href={""}
-        projectState={"ACTIVE"}
+        projectState={"DONE"}
         startDate={"TBD"}
         finishDate={"TBD"}
+        projectHref={
+          "https://glittery-sopapillas-89c59c.netlify.app/"
+        }
+        githubHref={
+          "https://github.com/ElijahMwambazi/Trillo"
+        }
+      />
+
+      {/* Project 5 */}
+      {/* TODO: Change description, add project href, add start date and finish date, change image*/}
+      <Project
+        imgSrc={
+          "../../public/projects/project-3.jpeg"
+        }
+        projectName={"Natour"}
+        description={
+          "The purpose of this project is to provide you with experience in mapping between phenomena in the real world (and data) and the possible distribution functions"
+        }
+        techStack={["HTML", "Sass", "JavaScript"]}
+        isActive={true}
+        projectState={"DONE"}
+        startDate={"TBD"}
+        finishDate={"TBD"}
+        projectHref={""}
+        githubHref={
+          "https://github.com/ElijahMwambazi/Natour"
+        }
+      />
+
+      {/* Project 6 */}
+      {/* TODO: Change description, add project href, add start date and finish date, change image*/}
+      <Project
+        imgSrc={
+          "../../public/projects/project-3.jpeg"
+        }
+        projectName={"Omnifood"}
+        description={
+          "The purpose of this project is to provide you with experience in mapping between phenomena in the real world (and data) and the possible distribution functions"
+        }
+        techStack={["HTML", "Sass", "JavaScript"]}
+        isActive={true}
+        projectState={"DONE"}
+        startDate={"TBD"}
+        finishDate={"TBD"}
+        projectHref={""}
+        githubHref={
+          "https://github.com/ElijahMwambazi/Omnifood"
+        }
       />
     </div>
   );
